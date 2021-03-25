30153365c6647c348d808f999e5eaff7da2bad18
Silas HW <silas.hayes.williams@gmail.com>
2021-03-25 19:30:01 +0000

    Update how checkpoints work

--- src/levels/Level1.gd
+++ src/levels/Level1.gd
@@ -3,9 +3,5 @@ extends Level
 func _ready():
 	RespawnLocation = $Respawn_Location
 	
-	CheckpointPositions = [
-	$Checkpoints/Checkpoint_1.position
-]
-
-func _on_Checkpoint_1_body_entered(body):
-	RespawnLocation.position = CheckpointPositions[0]
+func _on_Checkpoint_body_entered(body):
+	RespawnLocation.position = body.position
